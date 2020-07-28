a1463066b394b6e02ec44e5a2ef66f5e45120d0d
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-07-28 21:14:07 +0900

    Add devise_token_auth User

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
