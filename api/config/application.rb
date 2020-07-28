33f8e07bc30bb3f3ea7333219909f0dd06d293e7
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-07-28 21:55:31 +0900

    [Add] twitter認証用の設定ファイルの追加・編集

--- api/config/application.rb
+++ api/config/application.rb
@@ -31,5 +31,18 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_session_mechaco'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods: [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
