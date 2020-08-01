84be63a3b087c10713f3db0f565941dfd03fd5fc
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-01 14:06:37 +0900

    [Fix] render_data_or_redirectメソッドを修正

--- api/app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
+++ api/app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
@@ -30,19 +30,12 @@ class Api::V1::Auth::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCall
   end
 
   def render_data_or_redirect(message, data, user_data = {})
-    if Rails.env.production?
-      if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
-        render_data(message, user_data.merge(data))
-      elsif auth_origin_url
-        redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
-      else
-        fallback_render data[:error] || 'An error occurred'
-      end
+    if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
+      render_data(message, user_data.merge(data))
+    elsif auth_origin_url
+      redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
     else
-      # @resource.credentials = auth_hash["credentials"]
-
-      # わかりやすい様に開発時はjsonとして結果を返す
-      render json: @resource, status: :ok
+      fallback_render data[:error] || 'An error occurred'
     end
   end
 end
