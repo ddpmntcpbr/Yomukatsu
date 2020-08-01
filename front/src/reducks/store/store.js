a8faa132d0dbc12becc5939f3e8ca32f6d11fe43
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-01 13:50:24 +0900

    [Add] ダミーのログイン機能を追加

--- front/src/reducks/store/store.js
+++ front/src/reducks/store/store.js
@@ -4,14 +4,14 @@ import {
   applyMiddleware
 } from 'redux';
 import {connectRouter, routerMiddleware} from "connected-react-router";
-// import {UsersReducer} from "../users/reducers";
+import {UsersReducer} from "../users/reducers";
 import thunk from "redux-thunk";
 
 export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
       router: connectRouter(history),
-      // users: UsersReducer,
+      users: UsersReducer,
     }),
     applyMiddleware(
       routerMiddleware(history),
--- front/src/templates/Home.jsx
+++ front/src/templates/Home.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import {getUserId, getUserName} from '../reducks/users/selectors';
+import {useSelector, useDispatch} from 'react-redux'
+import {signIn} from "../reducks/users/operations"
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+  const username = getUserName(selector);
+
   return (
     <div>
-      <p>Home</p>
+      <h2>Home</h2>
+      <p>ユーザーID:{uid}</p>
+      <p>ユーザー名:{username}</p>
+      <button onClick={() => dispatch(signIn())}>SIGN IN</button>
     </div>
   );
 };
