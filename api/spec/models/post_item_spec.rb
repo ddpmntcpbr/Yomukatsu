cda9faa121e36995a3c4adedb60c33ca6dd8a69c
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-02 18:42:32 +0900

    [Add] postおよびpost_itemのモデルスペック作成

--- api/spec/models/post_item_spec.rb
+++ api/spec/models/post_item_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe PostItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "入力値が適切な場合" do
+    let(:post_item) { build(:post_item) }
+
+    it "新規作成できる" do
+      expect(post_item.valid?).to eq true
+    end
+  end
+
+  context "content が指定されていない場合" do
+    let(:post_item) { build(:post_item, content: nil) }
+
+    it "エラーする" do
+      post_item.valid?
+      expect(post_item.errors.messages[:content]).to include "can't be blank"
+    end
+  end
 end
--- api/spec/models/post_spec.rb
+++ api/spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "入力値が適切な場合" do
+    let(:post) { build(:post) }
+
+    it "新規作成できる" do
+      expect(post.valid?).to eq true
+    end
+  end
+
+  context "title が指定されていない場合" do
+    let(:post) { build(:post, title: nil) }
+
+    it "エラーする" do
+      post.valid?
+      expect(post.errors.messages[:title]).to include "can't be blank"
+    end
+  end
 end
