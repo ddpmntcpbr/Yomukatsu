cf69a4ffd3f656f8f4d292d56ae15dff5d369d0b
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-02 19:12:04 +0900

    [Add] postsコントローラーにアクションを定義

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,30 @@
-class Api::V1::PostsController < ApplicationController
+class Api::V1::PostsController < Api::V1::ApiController
+  before_action :authenticate_user!
+
   def show
+    post = current_user.post.find(params[:id])
+    render json: post
   end
+
+  def create
+    post = current_user.posts.create!(post_params)
+    render json: post
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    @post.update!(post_params)
+    render json: @post
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy!
+    render json: {}, status: :ok
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :url, :image, :status, post_items_attributes:[:content])
+    end
 end
--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :post_items, dependent: :destroy
+  accepts_nested_attributes_for :post_items
 
   validates :title, presence: true
 
