308060caa1465c608595f041fcafaadf1534bf6f
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-06 21:32:28 +0900

    [Remove] amazon-ecs gemの設定ファイルを削除

--- api/config/initializers/amazon_ecs.rb
+++
@@ -1,7 +0,0 @@
-require 'amazon/ecs'
-
-Amazon::Ecs.configure do |options|
-  options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
-  options[:AWS_secret_key] = ENV['AWS_SECRET_KEY']
-  options[:associate_tag] = ENV['AMAZON_ASSOCIATE_TAG']
-end
