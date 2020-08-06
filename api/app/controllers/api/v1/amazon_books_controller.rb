8ba6e484fc32e43b219366b20b258ff198945c41
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-06 20:00:48 +0900

    [Add] amazon_searchアクションを作成

--- api/app/controllers/api/v1/amazon_books_controller.rb
+++ api/app/controllers/api/v1/amazon_books_controller.rb
@@ -1,4 +1,29 @@
 class Api::V1::AmazonBooksController < ApplicationController
   def search
+    if params[:keyword].present?
+      #デバックログ出力するために記述
+      Amazon::Ecs.debug = true
+
+      # Amazon::Ecs::Responceオブジェクトの取得
+      amazon_books = Amazon::Ecs.item_search(
+        params[:keyword],
+        search_index:  'Books',
+        dataType: 'script',
+        response_group: 'ItemAttributes, Images',
+        country:  'jp',
+        power: "Not kindle"
+      )
+
+      # 本のタイトル,画像URL, 詳細ページURLの取得
+      @amazon_books = []
+      amazon_books.items.each do |item|
+        amazon_book = AmazonBook.new(
+          item.get('ItemAttributes/Title'),
+          item.get('LargeImage/URL'),
+          item.get('DetailPageURL'),
+        )
+        @amazon_books << amazon_book
+      end
+    end
   end
 end
--- api/config/initializers/amazon_ecs.rb
+++ api/config/initializers/amazon_ecs.rb
@@ -1,3 +1,5 @@
+require 'amazon/ecs'
+
 Amazon::Ecs.configure do |options|
   options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
   options[:AWS_secret_key] = ENV['AWS_SECRET_KEY']
