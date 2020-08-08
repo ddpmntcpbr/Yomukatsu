c4857a637968f4e0c3422cd24c47fe4e60aa026b
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-08 10:36:30 +0900

    [Fix] rubocop -a

--- api/app/controllers/api/v1/amazon_books_controller.rb
+++ api/app/controllers/api/v1/amazon_books_controller.rb
@@ -6,26 +6,27 @@ class Api::V1::AmazonBooksController < ApplicationController
   end
 
   private
-    def search_by_amazon(keyword)
-      request = Vacuum.new(marketplace: 'JP',
-        access_key: ENV['AMAZON_API_ACCESS_KEY'],
-        secret_key: ENV['AMAZON_API_SECRET_KEY'],
-        partner_tag: ENV['ASSOCIATE_TAG'])
-      response = request.search_items(keywords: keyword,
-                      search_index: 'Books',
-                      resources: ['ItemInfo.Title', 'Images.Primary.Large']).to_h
 
+    def search_by_amazon(keyword)
+      request = Vacuum.new(marketplace: "JP",
+                           access_key: ENV["AMAZON_API_ACCESS_KEY"],
+                           secret_key: ENV["AMAZON_API_SECRET_KEY"],
+                           partner_tag: ENV["ASSOCIATE_TAG"])
+      response = request.search_items(keywords: keyword,
+                                      search_index: "Books",
+                                      resources: ["ItemInfo.Title", "Images.Primary.Large"]).to_h
+      items = response.dig("SearchResult", "Items")
 
       # 検索結果から本のタイトル,画像URL, 詳細ページURLの取得して配列へ格納
       amazon_books = []
       items.each do |item|
-      amazon_book = {
-        title: item.dig('ItemInfo', 'Title', 'DisplayValue'),
-        image: item.dig('Images', 'Primary', 'Large', 'URL'),
-        url: item.dig('DetailPageURL')
-      }
-      amazon_books << amazon_book
-    end
-    amazon_books
+        amazon_book = {
+          title: item.dig("ItemInfo", "Title", "DisplayValue"),
+          image: item.dig("Images", "Primary", "Large", "URL"),
+          url: item.dig("DetailPageURL"),
+        }
+        amazon_books << amazon_book
+      end
+      amazon_books
     end
 end
