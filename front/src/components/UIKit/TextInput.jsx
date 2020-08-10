40b6b031b4bc2838c195dddb46ca3f458bd01ede
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-08-10 09:50:00 +0900

    [Add] TextInputコンポーネントを作成

---
+++ front/src/components/UIKit/TextInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField'
+
+const TextInput = (props) => {
+  return (
+    <TextField
+      fullWidth={props.fullWidth}
+      label={props.label}
+      margin={"dense"}
+      multiline={props.multiline}
+      required={props.required}
+      rows={props.rows}
+      value={props.value}
+      type={props.type}
+      onChange={props.onChange}
+    />
+  )
+}
+
+export default TextInput
