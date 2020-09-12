3a3a7e45ec9e08d4bf3f5a56fe2d3f4cfbc18eb8
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-09-12 21:24:24 +0900

    [Add] tab機能付きのマイページのモックを作成

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch} from "react-router";
-import {PostEdit, PostShow, TopPage, MyPage} from "./templates";
+import {PostEdit, PostShow, TopPage, MyTabPage, MyPage} from "./templates";
 import Auth from "./Auth"
 
 const Router = () => {
@@ -10,6 +10,7 @@ const Router = () => {
 
       <Auth>
         <Route exact path={"/mypage"} component={MyPage} />
+        <Route exact path={"/my_tab_page"} component={MyTabPage} />
         <Route exact path={"/posts/edit"} component={PostEdit} />
         <Route path={"/posts(/:id)?"} component={PostShow} />
       </Auth>
---
+++ front/src/templates/MyTabPage.jsx
@@ -0,0 +1,65 @@
+import React,{ useEffect,useState } from 'react';
+import {getUserId, getUserName, getUserImage} from '../reducks/users/selectors';
+import {useSelector, useDispatch} from 'react-redux'
+import {TwitterShareButton,TwitterIcon} from "react-share";
+import {push} from "connected-react-router";
+import {makeStyles} from "@material-ui/styles";
+import {AppBar,Avatar,Box,Container,Paper,Tab,Tabs,Typography} from '@material-ui/core';
+import { TabPanel,PrimaryButton } from "../components/UIkit";
+import {fetchPosts} from "../reducks/posts/operations";
+import {getPosts} from "../reducks/posts/selectors";
+import { BookCard } from "../components/UIkit";
+
+const useStyles = makeStyles((theme)=>({
+  root: {
+
+  }
+}))
+
+const MyTabPage = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector(state => state);
+
+  const uid = getUserId(selector);
+  const username = getUserName(selector);
+  const image = getUserImage(selector);
+  const posts = getPosts(selector);
+
+  const [selectedTab,setSelectedTab] = useState(0);
+
+  const handleChange = (event, newSelectedTab) => {
+    setSelectedTab(newSelectedTab)
+  };
+
+  useEffect(()=>{
+    dispatch(fetchPosts())
+  },[])
+
+  return(
+    <Container maxWidth="sm" >
+      <Paper>
+        <Box display="flex" flexDirection="row" >
+          <Avatar alt="User Icon" src={image} />
+          <Typography variant="h6" component="h2">
+            {username}さんのマイページ
+          </Typography>
+        </Box>
+      </Paper>
+
+      <Tabs value={selectedTab} onChange={handleChange}>
+        <Tab label="読書中" />
+        <Tab label="完読リスト" />
+      </Tabs>
+      <TabPanel value={selectedTab} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={selectedTab} index={1}>
+        Item Two
+      </TabPanel>
+
+    </Container>
+  )
+}
+
+export default MyTabPage
