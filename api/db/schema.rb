801ac5a3ca658d64137826dd2cac0d8228bbded1
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-09-16 21:11:49 +0900

    [Fix] DBをpostgresqlからMySQLに変更

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_08_02_085959) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "post_items", force: :cascade do |t|
+  create_table "post_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "post_id"
     t.datetime "created_at", null: false
@@ -23,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_085959) do
     t.index ["post_id"], name: "index_post_items_on_post_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.string "author"
@@ -35,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_085959) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,8 +43,8 @@ ActiveRecord::Schema.define(version: 2020_08_02_085959) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "nickname"
     t.string "image"
