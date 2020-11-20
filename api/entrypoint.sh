65f1503eeeb393ca401cd666d749c224dad6d9f1
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-11-21 06:38:08 +0900

    [Fix] entrypoint.shのファイル名を変更

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 # デバッグ用コメント
 echo "Start /api/entrypoint.sh"
-
-# Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bin/setup
+bundle exec pumactl start
