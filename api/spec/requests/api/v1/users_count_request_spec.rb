5fae37a3949e6a216dec8c9fcdf72ece082e6229
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-11-23 09:55:28 +0900

    [Fix] rubocop -a

--- api/spec/requests/api/v1/users_count_request_spec.rb
+++ api/spec/requests/api/v1/users_count_request_spec.rb
@@ -1,12 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::UsersCounts", type: :request do
-
   describe "GET /index" do
     it "returns http success" do
       get "/api/v1/users_count"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
