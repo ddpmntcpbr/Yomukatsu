e9ef84d3d44dd389891fa38c14d9645df9ace9bf
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-11-23 09:54:50 +0900

    [Fix] users_count のテストを修正

--- api/spec/requests/api/v1/users_count_request_spec.rb
+++ api/spec/requests/api/v1/users_count_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Api::V1::UsersCounts", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/api/v1/users_count/index"
+      get "/api/v1/users_count"
       expect(response).to have_http_status(:success)
     end
   end
