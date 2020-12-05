dc4f10c743dd049c980ffe241cdf787eaf012074
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-05 12:35:58 +0900

    [Add] RegisteredBookPageを編集

--- front/src/templates/RegisteredBookPage.jsx
+++ front/src/templates/RegisteredBookPage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
-import { Container,Typography } from "@material-ui/core"
+import { Box,Container,Typography } from "@material-ui/core"
 import { fetchRegisteredPosts } from "../reducks/posts/operations"
 import { makeStyles } from "@material-ui/styles";
 import { getPosts } from "../reducks/posts/selectors"
+import { BookCard } from "../components/UIkit"
+import { push } from "connected-react-router";
 
 const useStyles = makeStyles((theme)=>({
   root: {
@@ -23,7 +25,18 @@ const RegisteredBookPage = () => {
   return (
     <Container maxWidth="sm" >
       {posts.length > 0 ? (
-        <Typography>{posts[0].title}</Typography>
+        posts.map(post => (
+          <Box
+            key={post.id}
+            onClick={()=>dispatch(push("posts/show/" + String(post.id)))}
+          >
+            <BookCard
+              title={post.title}
+              author={post.author}
+              image={post.image}
+            />
+          </Box>
+        ))
       ) : (
         <Typography>読書中アイテムなし</Typography>
       )}
