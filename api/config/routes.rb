3f9cc79c45683bee9d6f2e2eaa349b2d3fb9024f
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-05 14:02:33 +0900

    [Add] Api::V1::Completed::Posts コントローラーを作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      namespace :registered do
-        get 'posts/index'
-        get 'posts/show'
-      end
-    end
-  end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
@@ -18,6 +11,10 @@ Rails.application.routes.draw do
       end
 
       namespace :registered do
+        resources :posts, only: [:index,:show]
+      end
+
+      namespace :completed do
         resources :posts, only: [:index,:show]
       end
 
