41995e0f432e3613b689e12dc3f1a1b9584ebf41
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-06 14:38:25 +0900

    [Add] isNonEmptyArray()を作成

--- front/src/helpers.js
+++ front/src/helpers.js
@@ -1,1 +1,11 @@
 export const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const isNonEmptyArray = (obj) => {
+  if(typeof obj === 'undefined'){
+    return false
+  } else if(obj.length === 0) {
+    return false
+  } else {
+    return true
+  }
+}
