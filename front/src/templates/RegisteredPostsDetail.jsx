b10dd0bb919a28d496abe767320571856c09e3b3
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-06 09:03:46 +0900

    [Fix] PostsDetailsの取得方法を変更

--- front/src/templates/RegisteredPostsDetail.jsx
+++ front/src/templates/RegisteredPostsDetail.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { Box,Container,Typography } from "@material-ui/core"
-import { fetchRegisteredPostsDetail } from "../reducks/posts/operations"
+// import { fetchRegisteredPostsDetail } from "../reducks/posts/operations"
 import { makeStyles } from "@material-ui/styles";
 import { getPosts } from "../reducks/posts/selectors"
+import axios from "axios"
 
 const useStyles = makeStyles((theme)=>({
   root: {
@@ -14,21 +15,28 @@ const RegisteredPostsDetail = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const selector = useSelector((state)=>state);
-  const posts = getPosts(selector);
   const path = selector.router.location.pathname;
   const id = path.split("/registered/posts/")[1];
+  const [post,setPost] = useState({});
 
   useEffect(()=>{
-    dispatch(fetchRegisteredPostsDetail(id))
+    const fetchPostsDetail = async () => {
+      const response = await axios.get((process.env.REACT_APP_API_V1_URL + '/registered/posts/' +  String(id)), {
+        headers: {
+          'access-token': localStorage.getItem('auth_token'),
+          'client': localStorage.getItem('client_id'),
+          'uid': localStorage.getItem('uid'),
+        }
+      })
+      setPost(response.data)
+    };
+    fetchPostsDetail()
   },[dispatch,id])
 
   return (
     <Container maxWidth="sm" >
-      <div>RegisteredPostDetail</div>
-      {posts.length > 0 ? (
-        <Typography>{posts[0].title}</Typography>
-      ) : (
-        <Typography>読書中アイテムなし</Typography>
+      {post && (
+        <Typography>{post.title}</Typography>
       )}
     </Container>
   )
