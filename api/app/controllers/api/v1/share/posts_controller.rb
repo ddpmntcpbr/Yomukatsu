82a78815e57e929a80fdb6a8488513e8ce534b29
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-18 19:25:01 +0900

    [Add] share/postsコントローラーに index、showアクションを追加

--- api/app/controllers/api/v1/share/posts_controller.rb
+++ api/app/controllers/api/v1/share/posts_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::Share::PostsController < ApplicationController
+
+  def index
+    posts = Post.order(updated_at: :desc)
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
 end
