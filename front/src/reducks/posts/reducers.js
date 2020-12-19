feb8eaef9efc7dbdf0f233538539e69fe9db8140
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-12-19 19:11:28 +0900

    [Add] fetchPosts()に対応するactions,reducers,selecotrsを作成

--- front/src/reducks/posts/reducers.js
+++ front/src/reducks/posts/reducers.js
@@ -3,42 +3,44 @@ import initialState from '../store/initialState'
 
 export const PostsReducer = (state = initialState.posts, action) => {
   switch (action.type) {
-    case Actions.FETCH_READING_POSTS:
-      return {
-        ...state,
-        isFetching: false,
-        list: [...action.payload]
-      };
-    case Actions.FETCH_COMPLETED_POSTS:
-      return {
-        ...state,
-        isFetching: false,
-        list: [...action.payload]
-      };
-    case Actions.FETCH_COMPLETED_POSTS_DETAIL:
-      return {
-        ...state,
-        isFetching: false,
-        list: [...action.payload]
-      };
+    // case Actions.FETCH_READING_POSTS:
+    //   return {
+    //     ...state,
+    //     isFetching: false,
+    //     list: [...action.payload]
+    //   };
+    // case Actions.FETCH_COMPLETED_POSTS:
+    //   return {
+    //     ...state,
+    //     isFetching: false,
+    //     list: [...action.payload]
+    //   };
+    // case Actions.FETCH_COMPLETED_POSTS_DETAIL:
+    //   return {
+    //     ...state,
+    //     isFetching: false,
+    //     list: [...action.payload]
+    //   };
     case Actions.FETCH_POSTS:
       return {
         ...state,
         isFetching: false,
-        list: [...action.payload]
+        reading: action.payload["reading"],
+        registered: action.payload["registered"],
+        completed: action.payload["completed"]
       };
-    case Actions.FETCH_REGISTERED_POSTS:
-      return {
-        ...state,
-        isFetching: false,
-        list: [...action.payload]
-      };
-    case Actions.FETCH_REGISTERED_POSTS_DETAIL:
-      return {
-        ...state,
-        isFetching: false,
-        list: [...action.payload]
-      };
+    // case Actions.FETCH_REGISTERED_POSTS:
+    //   return {
+    //     ...state,
+    //     isFetching: false,
+    //     list: [...action.payload]
+    //   };
+    // case Actions.FETCH_REGISTERED_POSTS_DETAIL:
+    //   return {
+    //     ...state,
+    //     isFetching: false,
+    //     list: [...action.payload]
+    //   };
     case Actions.START_FETCHING_POSTS:
       return {
         ...state,
