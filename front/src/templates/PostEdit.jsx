0f8b815af5e46a4276a48d08c949c19f6feb6887
ddpmntcpbr <ddpmntcpbr@gmail.com>
2021-01-02 10:21:17 +0900

    [Fix] react-final-formを一時コメントアウト

--- front/src/templates/PostEdit.jsx
+++ front/src/templates/PostEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback} from 'react';
-import { BookCard,PrimaryButton, TextInput } from "../components/UIkit"
+import { BookCard,PrimaryButton, SecondaryButton,TextInput } from "../components/UIkit"
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import {makeStyles} from "@material-ui/styles";
@@ -9,6 +9,10 @@ import {PostEditDialog,SearchResultDialog,SetMapArea} from "../components/Posts"
 import {Container,Box} from "@material-ui/core";
 import {saveReadingPost,saveRegisteredPost} from "../reducks/posts/operations";
 import {useDispatch} from "react-redux";
+import { Form, Field } from 'react-final-form'
+import arrayMutators from 'final-form-arrays'
+import { FieldArray } from 'react-final-form-arrays'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const useStyles = makeStyles((theme)=>({
   titleTypography: {
@@ -86,6 +90,10 @@ const PostEdit = () => {
     dispatch(saveRegisteredPost(title,url,author,image,mapItems))
   },[dispatch,title,url,author,image,mapItems])
 
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+
   return (
     <Container maxWidth="md" style={{backgroundColor:"white"}}>
       <Typography variant="h2" className={classes.titleTypography}>
@@ -113,13 +121,80 @@ const PostEdit = () => {
         読書メンタルマップ
       </Typography>
       <SetMapArea mapItems={mapItems} setMapItems={setMapItems}  />
+
+      {/* <Form
+        onSubmit={onSubmit}
+        mutators={{
+          ...arrayMutators
+        }}
+        render={({
+          handleSubmit,
+          form: {
+            mutators: { push, pop }
+          }, // injected from final-form-arrays above
+          pristine,
+          form,
+          submitting,
+          values
+        }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <FieldArray name="mapItems">
+                {({ fields }) =>
+                  fields.map((name, index) => (
+                    <div key={name}>
+                      <label>マップ{index + 1}</label>
+                      <Field
+                        name={`${name}.firstName`}
+                        component="input"
+                        placeholder="マップアイテムを入力"
+                      />
+                      <span
+                        onClick={() => fields.remove(index)}
+                      >
+                        <HighlightOffIcon/>
+                      </span>
+                    </div>
+                  ))
+                }
+              </FieldArray>
+
+              <Box>
+                <PrimaryButton
+                  label="マップアイテムを追加"
+                  onClick={() => push('mapItems', undefined)}
+                />
+              </Box>
+
+              <div className="buttons">
+                <button type="submit" disabled={submitting || pristine}>
+                  Submit
+                </button>
+              </div>
+              <Box>
+                <PrimaryButton
+                  label="保存"
+                  type="submit"
+                  onClick={() => push('mapItems', undefined)}
+                />
+              </Box>
+            </form>
+          )
+        }}
+      /> */}
+
+
+
+
       <Box textAlign="center" mt={4}>
-        <PrimaryButton
+        {/* <PrimaryButton
           label="POSTを登録！"
-          onClick={() => dispatch(saveRegisteredPost(title,url,author,image,mapItems))} />
-        <PrimaryButton
-          label="モーダルを開く"
-          onClick={handleSaveModalOpen} />
+          onClick={() => dispatch(saveRegisteredPost(title,url,author,image,mapItems))}
+        /> */}
+        <SecondaryButton
+          label="登録!"
+          onClick={handleSaveModalOpen}
+        />
       </Box>
       <PostEditDialog
         open={saveModalOpen}
