cd1a313eba06df361d0919b8ff78eea003dd390d
ddpmntcpbr <ddpmntcpbr@gmail.com>
2021-01-18 16:22:38 +0900

    [Fix] Footerナビゲーションのボタンを変更

--- front/src/components/ResponsiveBottomNavigation/ResponsiveBottomNavigation.jsx
+++ front/src/components/ResponsiveBottomNavigation/ResponsiveBottomNavigation.jsx
@@ -5,10 +5,10 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HelpIcon from '@material-ui/icons/Help';
 import HomeIcon from '@material-ui/icons/Home';
 import BookmarksIcon from '@material-ui/icons/Bookmarks';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import {useLocation} from 'react-router-dom';
 import { push } from "connected-react-router";
 import { useDispatch } from 'react-redux';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme)=>({
   wrapper:{
