1893147bf754469c2c51881d94affcb49d82a764
ddpmntcpbr <ddpmntcpbr@gmail.com>
2021-03-05 01:12:40 +0900

    [Fix] 正常系テスト

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import { CssBaseline,      Container } from '@material-ui/core'
+import { CssBaseline, Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import Router from 'Router'
 import { Header } from 'components/Header'
