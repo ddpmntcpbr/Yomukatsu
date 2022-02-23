cc69a2ddfcf3328fca059d8c47f93880bf1b1422
ddpmntcpbr <ddpmntcpbr@gmail.com>
2022-02-23 11:56:20 +0900

    herokuデプロイ用に設定を変更

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -5,6 +5,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 app_root = File.expand_path("..", __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 # stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
